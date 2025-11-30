efa4b4cdc1c8be5ec9ed8d7647d5aab70700ad94
Kamil Pietrzkiewicz <sargitcom@gmail.com>
2025-11-30 17:52:07 +0100

    add propper attribute for route of register user controller

--- src/Controller/RegisterUser/RegisterUserController.php
+++ src/Controller/RegisterUser/RegisterUserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\RegisterUser;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route("/user", name: "register_user", methods: ["POST"])]
 class RegisterUserController extends AbstractController
 {
     public function __invoke(RegisterUserRequest $request): RegisterUserResponse
